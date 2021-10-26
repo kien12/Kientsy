6473da9837a6fe704773f9f9e140f41b9a82d485
Kien Nguyen <nguyen_kien@live.com>
2021-10-25 22:45:59 -0700

    Finish user reducer

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,44 @@
+import * as SessionApiUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+//action creators
+// always has a type and data
+export const receiveCurrentUser = currentUser = ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+//thunk action creator is a function that is wrapped around an action creator and returns it
+export const signup = user => dispatch => (
+  SessionApiUtil.signup(user)
+    .then(
+      user => dispatch(receiveCurrentUser(user)),
+      err => dispatch(receiveErrors(err.responseJSON))
+    )
+  )
+
+export const login = user => dispatch => (
+  SessionApiUtil.login(user)
+    .then(
+      user => dispatch(receiveCurrentUser(user)),
+      error = dispatch(receiveErrors(error.responseJSON))
+  )
+)
+
+export const logout = () => dispatch => (
+  SessionApiUtil.logout()
+    .then(() => dispatch(logoutCurrentUser()))
+)
+
+
+
---
+++ frontend/reducers/entities_reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import usersReducer from './users_reducer';
+
+const entitiesReducer = combineReducer({
+  users: usersReducer
+})
+
+export default entitiesReducer;
