4e81af261a9480fc65bc7c1da7f52d6adb05a514
Kien Nguyen <nguyen_kien@live.com>
2021-10-26 15:25:20 -0700

    Complete ui modal

---
+++ frontend/components/navbar/greeting_container.js
@@ -0,0 +1,6 @@
+import { connect } from 'react-redux';
+
+import Greeting from './greeting';
+
+
+export default connect(null, null)(Greeting);
--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      openModal = false
+    }
   }
   render() {
     return (
       <div>
         <h1>This is the navbar</h1>
+        <button className="sign-in">Sign In</button>
       </div>
     )
   }
